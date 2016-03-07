a28e02859b3e483c26f7ebaf4a34c83b2a846eb5
Matthew Cuyar <matt@enctypeapparel.com>
2016-03-06 20:38:52 -0500

    refactored folder structure for index page

--- src/app/responder/index/layouts/index.js
+++ src/app/responder/index/layouts/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import _ from 'lodash';
 import Marionette from 'backbone.marionette';
 
-import template from '../views/index.hbs'
+import template from '../templates/index.hbs'
 
 export default Marionette.LayoutView.extend({
 
