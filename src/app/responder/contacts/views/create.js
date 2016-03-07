d4b8849b36fc68e2880c186113995580628a7d12
Matthew Cuyar <matt@enctypeapparel.com>
2016-03-06 21:51:57 -0500

    ui/css tweaks

--- src/app/responder/contacts/views/create.js
+++ src/app/responder/contacts/views/create.js
@@ -143,9 +143,6 @@ export default Marionette.ItemView.extend({
         }
     },
 
-    onDestroy: function(){
-    },
-
     cancelContact: function (e) {
         e.preventDefault();
         this.destroy();
